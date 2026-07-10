544848f60e8a5879054188595cf2115d441e08c6
Kazi Ahammad Ullah <ahammadcode@gmail.com>
2026-07-10 20:00:50 +0600

    implement user authentication and registration with JWT, including login and refresh token functionality

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import cookieParser from "cookie-parser";
 import express, { Application, Request, Response } from "express";
 import config from "./config";
 import cors from "cors";
+import { authRoutes } from "./modules/auth/auth.routes";
+import { userRoutes } from "./modules/user/user.routes";
 const app: Application = express();
 
 app.use(
@@ -15,6 +17,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World");
 });
